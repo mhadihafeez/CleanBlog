462991db1c67448225c9df4a8bcc887a268f718e
mhadihafeez <mhadihafeez@gmail.com>
2018-02-06 01:30:37 +0500

    html_integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index' , as: 'home'
+  get 'home/about' , to: 'home#about' , as: 'about'
+  get 'home/contact' , to: 'home#contact' , as: 'contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
